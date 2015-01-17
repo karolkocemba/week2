91bb02d8fd5ce0f4ae7fe282e04ba91bb4ed0ad2
karolkocemba <karol.kocemba@gmail.com>
2015-01-17 09:54:43 -0600

    solution 1 complete

--- solution_1.py
+++ solution_1.py
@@ -1,4 +1,12 @@
-def main():
+
+def fib(nth):
+	last = 0
+	total = 1
+	for i in range(nth):
+		total += last
+		last = total-last
+	return total
 
 
-main()
+n = eval(input("Enter a number, n, to see the nth Fibonacci number"))
+print (fib(n-1))
